862ffcb96840322f261e64c6fcba23096825c74f
Xin Zhang <xinzhang@Xins-MacBook-Pro.local>
2019-11-19 19:59:43 +1100

    update

--- simple/restclient.go
+++ simple/restclient.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+// there is a json-to-go tool to create the type
+type QuoteData struct {
+	Success struct {
+		Total int `json:"total"`
+	} `json:"success"`
+	Contents struct {
+		Quotes []struct {
+			Quote      string   `json:"quote"`
+			Length     string   `json:"length"`
+			Author     string   `json:"author"`
+			Tags       []string `json:"tags"`
+			Category   string   `json:"category"`
+			Date       string   `json:"date"`
+			Permalink  string   `json:"permalink"`
+			Title      string   `json:"title"`
+			Background string   `json:"background"`
+			ID         string   `json:"id"`
+		} `json:"quotes"`
+		Copyright string `json:"copyright"`
+	} `json:"contents"`
+}
 
 func main() {
 	response, err := http.Get("http://api.theysaidso.com/qod")
@@ -20,6 +43,12 @@ func main() {
 		return
 	}
 
-	fmt.Println(string(contents))
+	var result QuoteData
+	json.Unmarshal(contents, &result)
+	//fmt.Println(string(contents))
+	fmt.Println(result.Success.Total)
+	for _, q := range result.Contents.Quotes {
+		fmt.Println(q.Quote)
+	}
 
 }
